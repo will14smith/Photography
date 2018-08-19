9b5f2ea2eccc2a502158bf996bb143e6f1fa026f
Will Smith <will@toxon.co.uk>
2018-08-19 19:02:46 +0100

    Add react-router to admin

--- src/Toxon.Photography.Admin/src/index.tsx
+++ src/Toxon.Photography.Admin/src/index.tsx
@@ -4,18 +4,21 @@ import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-
-import Value from "./containers/Value";
 import reducer from "./redux/reducer";
 import registerServiceWorker from "./registerServiceWorker";
 
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./app";
 import "./index.css";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Value />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
