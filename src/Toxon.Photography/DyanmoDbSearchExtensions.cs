539cf2a1cbb5a8c748c6bb61fbccd858318da3a0
Will Smith <will@toxon.co.uk>
2018-08-19 12:23:19 +0100

    Created ListPhotographsFunction
    Closes #2

---
+++ src/Toxon.Photography/DyanmoDbSearchExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace Toxon.Photography
+{
+    public static class DyanmoDbSearchExtensions
+    {
+        public static async Task<IEnumerable<Document>> GetAllAsync(this Search search)
+        {
+            var documents = new List<Document>();
+
+            do
+            {
+                documents.AddRange(await search.GetNextSetAsync());
+            } while (!search.IsDone);
+
+            return documents;
+        }
+    }
+}
---
+++ src/Toxon.Photography/ListPhotographsFunction.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using Toxon.Photography.Http;
+using Toxon.Photography.Models;
+
+namespace Toxon.Photography
+{
+    public class ListPhotographsFunction
+    {
+        private readonly IAmazonDynamoDB _dynamoDb;
+
+        public ListPhotographsFunction()
+            : this(new AmazonDynamoDBClient())
+        {
+        }
+        public ListPhotographsFunction(IAmazonDynamoDB dynamoDb)
+        {
+            _dynamoDb = dynamoDb;
+        }
+
+        public async Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest request)
+        {
+            var photographTable = Table.LoadTable(_dynamoDb, TableNames.Photograph);
+            var search = photographTable.Scan(new ScanFilter());
+
+            var documents = await search.GetAllAsync();
+            var models = documents.Select(BuildModelFromDocument);
+
+            return BuildResponseFromModels(models);
+        }
+
+        internal static Photograph BuildModelFromDocument(Document document)
+        {
+            return new Photograph
+            {
+                Id = document["id"].AsGuid(),
+                Title = document["title"].AsString()
+            };
+        }
+
+
+        internal static APIGatewayProxyResponse BuildResponseFromModels(IEnumerable<Photograph> models)
+        {
+            var body = JsonConvert.SerializeObject(models.ToList());
+
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Headers = new Dictionary<string, string>
+                {
+                    ["Content-Type"] = "application/json",
+                },
+
+                Body = body,
+            };
+        }
+    }
+}
