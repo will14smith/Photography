596d924dc51e280d053732e1b9c28c87b8d29f1e
Will Smith <will@toxon.co.uk>
2018-08-27 10:49:49 +0100

    Handle sns message with stub thumbnail processor

---
+++ src/Toxon.Photography/ThumbnailProcessorFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.SNSEvents;
+using Newtonsoft.Json;
+using Toxon.Photography.Models;
+
+namespace Toxon.Photography
+{
+    public class ThumbnailProcessorFunction
+    {
+        public async Task Handle(SNSEvent snsEvent)
+        {
+            foreach (var record in snsEvent.Records)
+            {
+                var rawMessage = record.Sns.Message;
+                var message = JsonConvert.DeserializeObject<ImageProcessorMessage>(rawMessage);
+
+                await HandleMessageAsync(message);
+            }
+        }
+
+        private Task HandleMessageAsync(ImageProcessorMessage message)
+        {
+            Console.WriteLine("Handling thumbnail processor for: " + message.PhotographId);
+
+            return Task.CompletedTask;
+        }
+    }
+}
