8621b4f451410e312a8fcb9aad0c7e93c88369ad
Will Smith <will@toxon.co.uk>
2018-08-27 16:22:05 +0100

    Added GetPhotographFunction
    Closes #11

---
+++ src/Toxon.Photography/GetPhotographFunction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Toxon.Photography.Config;
+using Toxon.Photography.Http;
+using Toxon.Photography.Models;
+
+namespace Toxon.Photography
+{
+    public class GetPhotographFunction
+    {
+        private readonly IAmazonDynamoDB _dynamoDb;
+
+        public GetPhotographFunction()
+            : this(new AmazonDynamoDBClient())
+        {
+        }
+        public GetPhotographFunction(IAmazonDynamoDB dynamoDb)
+        {
+            _dynamoDb = dynamoDb;
+        }
+
+        public async Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest request)
+        {
+            var idStr = request.PathParameters["id"];
+            if (!Guid.TryParse(idStr, out var id))
+            {
+                return Response.CreateError(HttpStatusCode.BadRequest, "Invalid photograph id");
+            }
+
+            var photographTable = Table.LoadTable(_dynamoDb, TableNames.Photograph);
+            var document = await photographTable.GetItemAsync(id);
+            if (document == null)
+            {
+                return Response.CreateError(HttpStatusCode.NotFound, "No photograph with the requested id was found");
+            }
+
+            var model = ListPhotographsFunction.BuildPhotographFromDocument(document);
+
+            return BuildResponseFromModel(model);
+        }
+        
+        internal static APIGatewayProxyResponse BuildResponseFromModel(Photograph model)
+        {
+            return Response.CreateJson(HttpStatusCode.OK, model);
+        }
+    }
+}
