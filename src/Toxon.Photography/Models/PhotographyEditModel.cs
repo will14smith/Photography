df5428472885502f77066e480d694613d32c6a95
Will Smith <will@toxon.co.uk>
2018-09-01 12:27:27 +0100

    Added PUT /photograph/{id} endpoint
    Closes #16

---
+++ src/Toxon.Photography/Models/PhotographyEditModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Toxon.Photography.Models
+{
+    public class PhotographyEditModel
+    {
+        public string Title { get; set; }
+
+        public DateTime CaptureTime { get; set; }
+    }
+}
