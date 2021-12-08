99ee621e1d5fbf4ff9b7c0ad30723cec6b568e16
Will Smith <will@toxon.co.uk>
2021-12-08 21:57:10 +0000

    add backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "remote" {
+    hostname     = "will14smith.scalr.io"
+    organization = "env-tq0mi1vq39nn76o"
+    workspaces {
+      name = "photography"
+    }
+  }
+}
+
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
